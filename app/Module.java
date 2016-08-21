35df834fa6209e89ddb6717012e3315e3125a8f4
Prashant Sable <Prashant7090@gmail.com>
2016-08-22 00:04:12 +0530

    Removed unwanted code.

--- app/Module.java
+++ app/Module.java
@@ -27,8 +27,6 @@ public class Module extends AbstractModule {
         // Ask Guice to create an instance of ApplicationTimer when the
         // application starts.
         bind(ApplicationTimer.class).asEagerSingleton();
-        // Set AtomicCounter as the implementation for Counter.
-        bind(Counter.class).to(AtomicCounter.class);
 
         bind(DaoProvider.class).to(EbeanDaoProvider.class).in(Singleton.class);
     }
